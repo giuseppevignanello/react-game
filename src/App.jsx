48630c58395f2e9fad882db5857565f0b555edcb
giuseppevignanello <peppe.vignanello@gmail.com>
2023-07-26 17:09:55 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+
+const App = () => {
+  const [playerPosition, setPlayerPosition] = useState([0, 0]);
+
+  useEffect(() => {
+
+    window.addEventListener('keydown', handleKeyDown);
+
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+
+  const handleKeyDown = (event) => {
+    switch (event.key) {
+      case 'ArrowUp':
+        setPlayerPosition((prevPosition) => [prevPosition[0] - 5, prevPosition[1]]);
+        break;
+      case 'ArrowDown':
+        setPlayerPosition((prevPosition) => [prevPosition[0] + 5, prevPosition[1]]);
+        break;
+      case 'ArrowLeft':
+        setPlayerPosition((prevPosition) => [prevPosition[0], prevPosition[1] - 5]);
+        break;
+      case 'ArrowRight':
+        setPlayerPosition((prevPosition) => [prevPosition[0], prevPosition[1] + 5]);
+        break;
+      default:
+        break;
+    }
+  };
+
+  return (
+    <div>
+      <div className="container">
+        <div className="game_arena position-relative">
+
+          <div className="player cube position-absolute"
+            style={{ top: `${playerPosition[0]}%`, left: `${playerPosition[1]}%` }}
+          ></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
